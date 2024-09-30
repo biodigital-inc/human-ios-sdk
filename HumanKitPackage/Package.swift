0ea26150a5dc37c6f21af2baa2eed42dfbdffb47
Gary Herman <gary@biodigital.com>
2024-09-30 10:31:49 -0400

    spm process

--- HumanKitPackage/Package.swift
+++ HumanKitPackage/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "human-kit",
+    platforms: [
+            .macOS(.v11),
+            .iOS(.v12),
+        ],
     products: [
         .library(
             name: "human-kit",
