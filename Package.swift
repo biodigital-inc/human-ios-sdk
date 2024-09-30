8c4340bab05b72695281c6ed1c87aa29e5a7aee7
Gary Herman <gary@biodigital.com>
2024-09-30 11:05:10 -0400

    package

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "human-kit",
+    platforms: [
+            .macOS(.v13),
+            .iOS(.v15),
+        ],
+    products: [
+        .library(
+            name: "human-kit",
+            targets: ["human-kit", "HumanKit"]),
+    ],
+    targets: [
+        .target(
+            name: "human-kit"),
+        .binaryTarget(name: "HumanKit", path: "./HumanKit.xcframework")
+    ]
+)
