540479df1147fc10863e1070abba031c1e9cabe2
geeherman <gherman@cs.stanford.edu>
2025-05-05 12:28:52 -0400

    updated package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "HumanKitPackage",
-    sources: "HumanKitPackage",
     platforms: [
         .iOS(.v13)
     ],
@@ -13,15 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HumanKitPackage",
-            targets: ["HumanKitPackage", "HumanKit"])
+            targets: ["HumanKit"])
     ],
     targets: [
-        .target(
-            name: "HumanKitPackage"
-        ),
         .binaryTarget(
             name: "HumanKit",
-            path: "HumanKit.xcframework"
+            path: "Sources/HumanKitPackage/HumanKit.xcframework"
         )
     ]
 )
