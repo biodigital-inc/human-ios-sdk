322b38c0fcdb2a1c88919f78fc416830def16182
geeherman <gherman@cs.stanford.edu>
2025-05-05 11:35:54 -0400

    new swift package

--- HumanKitPackage/Package.swift
+++ HumanKitPackage/Package.swift
@@ -1,22 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "human-kit",
+    name: "HumanKitPackage",
     platforms: [
-            .macOS(.v11),
-            .iOS(.v12),
-        ],
+        .iOS(.v13)
+    ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "human-kit",
-            targets: ["human-kit", "HumanKit"]),
+            name: "HumanKitPackage",
+            targets: ["HumanKitPackage", "HumanKit"])
     ],
     targets: [
         .target(
-            name: "human-kit"),
-        .binaryTarget(name: "HumanKit", path: "../HumanKit.xcframework")
+            name: "HumanKitPackage"
+        ),
+        .binaryTarget(
+            name: "HumanKit",
+            path: "./HumanKit.xcframework"
+        )
     ]
 )
